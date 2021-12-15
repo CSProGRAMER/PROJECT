607a74ab0bd62277d31f83d51faef06c55736b5f
CSProGRAMER <96209347+CSProGRAMER@users.noreply.github.com>
2021-12-15 23:55:39 +0200

    Add files via upload

---
+++ PROJECT/readbin.m
@@ -0,0 +1,22 @@
+function [frame] = readbin (name, width, hight)
+fileID = fopen (name, 'r'); % data-fread(fileID);
+imageWidth = width; 
+imageHeight = hight; 
+numColor = 1;
+newData = zeros(imageHeight, imageWidth); 
+newData=im2double(newData);
+l=1;
+count = 1;
+[v, count2] = fscanf(fileID, '%f');
+for i = 1:imageHeight %for i 0 to maxColumn 
+        for j = 1:imageWidth % for j 0 to maxRow 
+            for k= 1:numColor
+            newData(i,j,1)=v(count);
+            count =count+1; 
+            end
+            l=1+1;
+        end
+end
+fclose(fileID);
+frame=newData;
+end
---
+++ PROJECT/savebin.m
@@ -0,0 +1,35 @@
+function []= savebin (yframe, cbframe, crframe, index)
+%UNTITLED2 Summary of this function goes here
+%Detailed explanation goes here
+fileID= fopen(strcat("videocam\savedY", int2str(index), "bin"), 'w');
+for r= 1:size (yframe, 1)
+    for c= 1:size (yframe, 2)
+        fprintf(fileID,"%f\n",yframe(r,c,1));
+    end
+    fprintf(fileID,"\n");
+end
+fclose(fileID); 
+imtool(yframe)
+
+%%%%
+fileID= fopen(strcat("videocam\savedcb", int2str(index), "bin"), 'w');
+for r= 1:size (cbframe, 1)
+    for c= 1:size (cbframe, 2)
+        fprintf(fileID,"%f\n",cbframe(r,c,1));
+    end
+    fprintf(fileID,"\n");
+end
+fclose(fileID); 
+imtool(cbframe)
+
+%%%%
+
+fileID= fopen(strcat("videocam\savedcr", int2str(index), "bin"), 'w');
+for r= 1:size (crframe, 1)
+    for c= 1:size (crframe, 2)
+        fprintf(fileID,"%f\n",crframe(r,c,1));
+    end
+    fprintf(fileID,"\n");
+end
+fclose(fileID); 
+imtool(crframe)
