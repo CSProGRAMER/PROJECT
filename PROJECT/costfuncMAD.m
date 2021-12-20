6979bc2c158dce086453bacebbfbb3dad9587be7
CSProGRAMER <96209347+CSProGRAMER@users.noreply.github.com>
2021-12-20 22:58:52 +0200

    Add files via upload

---
+++ PROJECT/costfuncMAD.m
@@ -0,0 +1,11 @@
+function cost = costfuncMAD(currentBlk, refBlk, n)
+
+err = 0; 
+for i = 1:n
+    for j = 1:n
+        result= result + abs((currentBlk(i,j)-refBlk(i,j)));
+    end
+end
+
+cost = result/(n*n);
+end
